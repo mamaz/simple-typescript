3b5d8f4a528c96677f7ad79621d8be241c353cb8
mamaz <hisma.sudradjat@btpn.com>
2018-07-17 01:22:17 +0700

    Initial commit

---
+++ dist/routes/services.route.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+  /services
+*/
+router.get('/', function(req, res, next) {
+  res.send('/services');
+});
+
+module.exports = router;
