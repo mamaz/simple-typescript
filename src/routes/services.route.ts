8e27b66d1cb2b66c8b754f0e52880bd18c003043
mamaz <hisma.sudradjat@btpn.com>
2018-07-21 20:56:36 +0700

    Fix lint issue

--- src/routes/services.route.ts
+++ src/routes/services.route.ts
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import * as express from "express";
+const router = express.Router();
 
 /*
   /services
 */
-router.get('/', function(req, res, next) {
-  res.send('/services');
+router.get("/", (req, res, next) => {
+  res.send("/services");
 });
 
 module.exports = router;
